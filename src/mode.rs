b932db240f06854a62ddfa23a46c1bf36bea522d
andrewtc <andrew@andrewtc.com>
2019-02-08 15:24:53 -0800

    Fix typo in `Mode` documentation

--- src/mode.rs
+++ src/mode.rs
@@ -80,8 +80,8 @@ use crate::Transition;
 pub trait Mode : 'static {
     /// Represents the user-facing interface for the `Mode` that will be exposed via the `Automaton`. In order to be
     /// used with an `Automaton`, the `Base` type of the `Mode` **must** match the `Base` type of the `Automaton`. This
-    /// is so that the `Automaton` can provide `get_mode()` and `get_mode_mut()` functions that return a reference to
-    /// the `Mode` as the `Base` type.
+    /// is so that the `Automaton` can provide `borrow_mode()` and `borrow_mode_mut()` functions that return a reference
+    /// to the `Mode` as the `Base` type.
     /// 
     type Base : ?Sized;
 
